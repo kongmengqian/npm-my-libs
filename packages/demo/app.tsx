017bec54c3a4dee12ef7348d3b5dd8bd453bf2c0
kongmengqian <1196134067@qq.com>
2020-07-27 16:08:34 +0800

    chore

--- packages/demo/app.tsx
+++ packages/demo/app.tsx
@@ -1,27 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Button, message } from "antd";
-import A from "@/a";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button, message } from 'antd';
+import A from '@/a';
 
-interface Props {}
-
-class App extends React.Component<Props, any, any> {
-  public constructor(props: Props) {
+class App extends React.Component<any, any, any> {
+  public constructor(props: any) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    message.success("你点了点我光秃秃的脑门");
+    message.success('你点了点我光秃秃的脑门');
   }
 
   componentDidMount() {
-    console.log("componentDidMount");
+    console.log('componentDidMount');
   }
 
   render() {
     return (
-      <div style={{ textAlign: "center" }}>
+      <div style={{ textAlign: 'center' }}>
         <h1>hello world!</h1>
         <A />
         <div>
@@ -33,5 +31,5 @@ class App extends React.Component<Props, any, any> {
     );
   }
 }
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 export default App;
